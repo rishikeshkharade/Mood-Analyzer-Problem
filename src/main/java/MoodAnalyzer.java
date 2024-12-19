abf857e8c803b267197fcfb0e277bacaef1ae54a
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-12-19 14:08:32 +0530

    UC1 Mood Analyse Happy or Sad

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,5 +1,32 @@
 public class MoodAnalyzer {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser Problem");
+        private String mood;
+        public MoodAnalyzer(String mood){
+            this.mood=mood;
+        }
+
+        public String analyseMood(){
+            if (mood==null|| mood.isEmpty()){
+                return "Neutral";
+            }
+            if (mood.toLowerCase().contains("happy")){
+                return "Happy";
+            } else if (mood.toLowerCase().contains("sad")) {
+                return "Sad";
+            }else {
+                return "Neutral";
+            }
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Mood Analyser Problem");
+
+            MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer("Happy Mood");
+            System.out.println("Mood: "+moodAnalyzer1.analyseMood());
+
+            MoodAnalyzer moodAnalyzer2 = new MoodAnalyzer("Sad Mood");
+            System.out.println("Mood: "+moodAnalyzer2.analyseMood());
+
+            MoodAnalyzer moodAnalyzer3 = new MoodAnalyzer("");
+            System.out.println("Mood: "+moodAnalyzer3.analyseMood());
+        }
     }
-}
