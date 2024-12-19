ee3977ef8e87f55c204887eb87f4ae2393a6a2e2
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-12-19 15:24:50 +0530

    TC1.2 Any mood should return Happy

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -9,4 +9,12 @@ public class MoodAnalyzerTest {
         String result = moodAnalyzer.analyseMood();
         assertEquals("SAD",result);
     }
+
+    @Test
+    void Mood(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Any Mood");
+        String result = moodAnalyzer.analyseMood();
+        assertEquals("HAPPY",result);
+    }
+
 }
